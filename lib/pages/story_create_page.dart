344a3ef367b89cae684a18e4efbf81a36c4bc04b
hippieperm <leess0133@gmail.com>
2025-04-23 20:03:54 +0900

    feat: 스토리 생성 페이지에 이미지 선택 기능 추가

--- lib/pages/story_create_page.dart
+++ lib/pages/story_create_page.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class StoryCreatePage extends StatelessWidget {
+class StoryCreatePage extends StatefulWidget {
   const StoryCreatePage({super.key});
+
+  @override
+  State<StoryCreatePage> createState() => _StoryCreatePageState();
+}
+
+class _StoryCreatePageState extends State<StoryCreatePage> {
+  final ImagePicker _picker = ImagePicker();
+  List<XFile> _imageFiles = [];
+
+  Future<void> _pickImages() async {
+    final List<XFile> selectedImages = await _picker.pickMultiImage();
+    if (selectedImages.isNotEmpty) {
+      setState(() {
+        _imageFiles = selectedImages;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +38,84 @@ class StoryCreatePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // 이미지 선택 영역
+            Container(
+              height: 120,
+              margin: EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                children: [
+                  // 이미지 추가 버튼
+                  GestureDetector(
+                    onTap: _pickImages,
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(color: Colors.grey[300]!),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.camera_alt,
+                              size: 32, color: Colors.grey[600]),
+                          SizedBox(height: 8),
+                          Text('사진 추가',
+                              style: TextStyle(color: Colors.grey[600])),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // 선택된 이미지 표시
+                  Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: _imageFiles.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 100,
+                          height: 100,
+                          margin: EdgeInsets.only(left: 8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            image: DecorationImage(
+                              image: FileImage(File(_imageFiles[index].path)),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          child: Stack(
+                            alignment: Alignment.topRight,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _imageFiles.removeAt(index);
+                                  });
+                                },
+                                child: Container(
+                                  margin: EdgeInsets.all(4),
+                                  padding: EdgeInsets.all(2),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black54,
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: Icon(Icons.close,
+                                      size: 18, color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
             TextField(
               decoration: InputDecoration(
                 hintText: '내용을 입력하세요',
