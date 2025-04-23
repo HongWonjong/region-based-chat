02c5c18cf7375899a112bf5e77cf0b83eb9e0a28
hippieperm <leess0133@gmail.com>
2025-04-23 19:51:48 +0900

    feat: 스토리 생성 페이지 기본 UI 구현

--- lib/pages/story_create_page.dart
+++ lib/pages/story_create_page.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 
-class WelcomePage extends StatelessWidget {
-  const WelcomePage({super.key});
+class StoryCreatePage extends StatelessWidget {
+  const StoryCreatePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '소문내기',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: '소문을 입력하세요',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: '내용을 입력하세요',
+              ),
+            ),
+            Spacer(),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  elevation: 2,
+                  shadowColor: Colors.blue.shade200,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(11),
+                  ),
+                ),
+                child: Text('소문내기'),
+              ),
+            ),
+            SizedBox(height: 40)
+          ],
+        ),
+      ),
+    );
   }
 }
