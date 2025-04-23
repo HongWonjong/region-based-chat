a8ea3266b179ca2f0b58a1867694e3b0442fd1ab
wonjonghong <wonhong1996@naver.com>
2025-04-23 10:19:49 +0900

    기초 커밋

---
+++ README.md
@@ -0,0 +1,2 @@
+# region-based-chat
+스파르타 코딩 클럽 1조 팀 프로젝트
---
+++ android/app/src/main/kotlin/com/example/region_based_chat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.region_based_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
