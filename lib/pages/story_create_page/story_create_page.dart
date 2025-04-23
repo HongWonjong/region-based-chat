81e6b6f302ef3d173116c1f5eca0614be4ffc7a5
hippieperm <leess0133@gmail.com>
2025-04-23 20:57:50 +0900

    feat: 스토리 생성 페이지의 입력 필드 및 제출 버튼 컴포넌트 통합, 불필요한 코드 제거

--- lib/pages/story_create_page/story_create_page.dart
+++ lib/pages/story_create_page/story_create_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'widgets/select_image_button.dart';
+import 'widgets/input_fields.dart';
+import 'widgets/submit_button.dart';
+import 'widgets/category_button.dart';
 
 class StoryCreatePage extends StatelessWidget {
   const StoryCreatePage({super.key});
@@ -20,137 +23,33 @@ class StoryCreatePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SelectImageButton(),
-            input(),
+            InputFields(),
             Spacer(),
             Row(
               children: [
-                categoryButton(text: '일반적인 행사'),
+                CategoryButton(text: '일반적인 행사'),
                 SizedBox(width: 10),
-                categoryButton(
-                    color: Color(0xFF77FA7D), text: '가벼운사건', isSelected: true),
+                CategoryButton(
+                  color: Color(0xFF77FA7D),
+                  text: '가벼운사건',
+                  isSelected: true,
+                ),
               ],
             ),
             SizedBox(height: 16),
             Row(
               children: [
-                categoryButton(text: '중요한 사건/범죄'),
+                CategoryButton(text: '중요한 사건/범죄'),
                 SizedBox(width: 10),
-                categoryButton(text: '분실'),
+                CategoryButton(text: '분실'),
               ],
             ),
             Spacer(),
-            submitButton(),
+            SubmitButton(),
             SizedBox(height: 40)
           ],
         ),
       ),
     );
   }
-
-  Column input() {
-    return Column(
-      children: [
-        TextField(
-          decoration: InputDecoration(
-            hintText: '내용을 입력하세요',
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(18)),
-            contentPadding: EdgeInsets.all(16),
-          ),
-        ),
-        SizedBox(height: 16),
-        TextField(
-          maxLines: 12,
-          decoration: InputDecoration(
-            hintText: '내용을 입력하세요',
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(18)),
-            contentPadding: EdgeInsets.all(16),
-          ),
-        ),
-      ],
-    );
-  }
-
-  SizedBox submitButton() {
-    return SizedBox(
-      width: double.infinity,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          elevation: 2,
-          shadowColor: Colors.blue.shade200,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(11),
-          ),
-        ),
-        child: Text(
-          '소문내기',
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Expanded categoryButton(
-      {Color? color, String text = '소문 카테고리', bool isSelected = false}) {
-    return Expanded(
-      child: SizedBox(
-        height: 50,
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            backgroundColor: color ?? Colors.grey, //TODO: 카테고리 선택 시 색상 변경
-            foregroundColor: Colors.black,
-            elevation: 0,
-            shadowColor: Colors.transparent,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            side: BorderSide(
-              color: isSelected ? Colors.black : Colors.transparent,
-              width: 2,
-            ),
-          ),
-          child: Text(
-            text,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-SizedBox submitButton() {
-  return SizedBox(
-    width: double.infinity,
-    height: 50,
-    child: ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        elevation: 2,
-        shadowColor: Colors.blue.shade200,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(11),
-        ),
-      ),
-      child: Text(
-        '소문내기',
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-  );
 }
