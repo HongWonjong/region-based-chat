d7ad9769af8a33b2ffc7498e77c4372dd4af75ea
hippieperm <leess0133@gmail.com>
2025-04-24 09:48:44 +0900

    feat: 카테고리 버튼을 StatefulWidget으로 변경하고 선택 상태 관리 기능 추가

--- lib/pages/story_create_page/widgets/category_button.dart
+++ lib/pages/story_create_page/widgets/category_button.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
-class CategoryButton extends StatelessWidget {
+class CategoryButton extends StatefulWidget {
   final Color? color;
+  final Color? activeColor;
   final String text;
-  final bool isSelected;
-  final VoidCallback? onPressed;
+  final bool initiallySelected;
+  final Function(bool)? onSelectionChanged;
 
   const CategoryButton({
     super.key,
     this.color,
+    this.activeColor,
     this.text = '소문 카테고리',
-    this.isSelected = false,
-    this.onPressed,
+    this.initiallySelected = false,
+    this.onSelectionChanged,
   });
+
+  @override
+  State<CategoryButton> createState() => _CategoryButtonState();
+}
+
+class _CategoryButtonState extends State<CategoryButton> {
+  late bool _isSelected;
+
+  @override
+  void initState() {
+    super.initState();
+    _isSelected = widget.initiallySelected;
+  }
+
+  @override
+  void didUpdateWidget(CategoryButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.initiallySelected != widget.initiallySelected) {
+      _isSelected = widget.initiallySelected;
+    }
+  }
+
+  void _handleTap() {
+    if (!_isSelected && widget.onSelectionChanged != null) {
+      widget.onSelectionChanged!(true);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +49,11 @@ class CategoryButton extends StatelessWidget {
       child: SizedBox(
         height: 50,
         child: ElevatedButton(
-          onPressed: onPressed ?? () {},
+          onPressed: _handleTap,
           style: ElevatedButton.styleFrom(
-            backgroundColor: color ?? Colors.grey,
+            backgroundColor: _isSelected
+                ? widget.activeColor
+                : (widget.color ?? Colors.grey),
             foregroundColor: Colors.black,
             elevation: 0,
             shadowColor: Colors.transparent,
@@ -30,12 +61,12 @@ class CategoryButton extends StatelessWidget {
               borderRadius: BorderRadius.circular(16),
             ),
             side: BorderSide(
-              color: isSelected ? Colors.black : Colors.transparent,
+              color: _isSelected ? Colors.black : Colors.transparent,
               width: 2,
             ),
           ),
           child: Text(
-            text,
+            widget.text,
             style: TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.bold,
