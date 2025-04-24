503602af5f52b5e1072d51f65a37586fd131366b
hippieperm <leess0133@gmail.com>
2025-04-24 21:05:08 +0900

    refactor: CategoryButton 위젯의 레이아웃 최적화 및 코드 정리

--- lib/pages/story_create_page/widgets/category_button.dart
+++ lib/pages/story_create_page/widgets/category_button.dart
@@ -45,32 +45,29 @@ class _CategoryButtonState extends State<CategoryButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: SizedBox(
-        height: 50,
-        child: ElevatedButton(
-          onPressed: _handleTap,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: _isSelected
-                ? widget.activeColor
-                : (widget.color ?? Colors.grey),
-            foregroundColor: Colors.black,
-            elevation: 0,
-            shadowColor: Colors.transparent,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            side: BorderSide(
-              color: _isSelected ? Colors.black : Colors.transparent,
-              width: 2,
-            ),
+    return SizedBox(
+      height: 50,
+      child: ElevatedButton(
+        onPressed: _handleTap,
+        style: ElevatedButton.styleFrom(
+          backgroundColor:
+              _isSelected ? widget.activeColor : (widget.color ?? Colors.grey),
+          foregroundColor: Colors.black,
+          elevation: 0,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
           ),
-          child: Text(
-            widget.text,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
+          side: BorderSide(
+            color: _isSelected ? Colors.black : Colors.transparent,
+            width: 2,
+          ),
+        ),
+        child: Text(
+          widget.text,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
