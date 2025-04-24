5c492be27f105becfdbb7f4cad152d7541fdf6a3
ghghtls <ghghtls2@gmail.com>
2025-04-24 18:48:10 +0900

    fix:인포추가

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:region_based_chat/pages/chat_page/chat_page.dart';
 import 'package:region_based_chat/pages/welcome_page/welcome_page.dart';
 import 'firebase_options.dart';
 
@@ -39,49 +38,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'firebase_options.dart';
-// import 'pages/auth/custom_drawer.dart'; // Drawer 파일 경로에 맞게 수정
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   // .env.dev 파일 로딩
-//   await dotenv.load(fileName: ".env.dev");
-
-//   // Firebase 초기화
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-
-//   runApp(const ProviderScope(child: MyApp()));
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: MainPage(),
-//     );
-//   }
-// }
-
-// class MainPage extends StatelessWidget {
-//   const MainPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text("테스트 로그인")),
-//       drawer: const CustomDrawer(), // 로그인 드로어
-//       body: const Center(child: Text("메인 콘텐츠 (지도 등)")),
-//     );
-//   }
-// }
