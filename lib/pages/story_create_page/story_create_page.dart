622d0343795e1f4d1ef9f418cb51593dcb3c346b
hippieperm <leess0133@gmail.com>
2025-04-24 09:54:07 +0900

    feat: 스토리 생성 페이지에 GestureDetector 추가하여 터치 시 포커스 해제 기능 구현

--- lib/pages/story_create_page/story_create_page.dart
+++ lib/pages/story_create_page/story_create_page.dart
@@ -31,55 +31,60 @@ class _StoryCreatePageState extends State<StoryCreatePage> {
                   fontWeight: FontWeight.bold))),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SelectImageButton(),
-            InputFields(),
-            Spacer(),
-            Row(
-              children: [
-                CategoryButton(
-                  text: '일반적인 행사',
-                  color: Colors.grey[300],
-                  activeColor: Color(0xffEEFF06).withAlpha(76),
-                  initiallySelected: _selectedCategoryIndex == 0,
-                  onSelectionChanged: (_) => _selectCategory(0),
-                ),
-                SizedBox(width: 10),
-                CategoryButton(
-                  color: Colors.grey[300],
-                  activeColor: Color(0xFF77FA7D).withAlpha(76),
-                  text: '가벼운사건',
-                  initiallySelected: _selectedCategoryIndex == 1,
-                  onSelectionChanged: (_) => _selectCategory(1),
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            Row(
-              children: [
-                CategoryButton(
-                  text: '중요한 사건/범죄',
-                  color: Colors.grey[300],
-                  activeColor: Color(0xffF20C0C).withAlpha(76),
-                  initiallySelected: _selectedCategoryIndex == 2,
-                  onSelectionChanged: (_) => _selectCategory(2),
-                ),
-                SizedBox(width: 10),
-                CategoryButton(
-                  text: '분실',
-                  color: Colors.grey[300],
-                  activeColor: Color(0xff0022FF).withAlpha(76),
-                  initiallySelected: _selectedCategoryIndex == 3,
-                  onSelectionChanged: (_) => _selectCategory(3),
-                ),
-              ],
-            ),
-            Spacer(),
-            SubmitButton(),
-            SizedBox(height: 40)
-          ],
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SelectImageButton(),
+              InputFields(),
+              Spacer(),
+              Row(
+                children: [
+                  CategoryButton(
+                    text: '일반적인 행사',
+                    color: Colors.grey[300],
+                    activeColor: Color(0xffEEFF06).withAlpha(76),
+                    initiallySelected: _selectedCategoryIndex == 0,
+                    onSelectionChanged: (_) => _selectCategory(0),
+                  ),
+                  SizedBox(width: 10),
+                  CategoryButton(
+                    color: Colors.grey[300],
+                    activeColor: Color(0xFF77FA7D).withAlpha(76),
+                    text: '가벼운사건',
+                    initiallySelected: _selectedCategoryIndex == 1,
+                    onSelectionChanged: (_) => _selectCategory(1),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  CategoryButton(
+                    text: '중요한 사건/범죄',
+                    color: Colors.grey[300],
+                    activeColor: Color(0xffF20C0C).withAlpha(76),
+                    initiallySelected: _selectedCategoryIndex == 2,
+                    onSelectionChanged: (_) => _selectCategory(2),
+                  ),
+                  SizedBox(width: 10),
+                  CategoryButton(
+                    text: '분실',
+                    color: Colors.grey[300],
+                    activeColor: Color(0xff0022FF).withAlpha(76),
+                    initiallySelected: _selectedCategoryIndex == 3,
+                    onSelectionChanged: (_) => _selectCategory(3),
+                  ),
+                ],
+              ),
+              Spacer(),
+              SubmitButton(),
+              SizedBox(height: 40)
+            ],
+          ),
         ),
       ),
     );
