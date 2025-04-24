2c3b60de28d32d427bba7adba9733cfe74cbe162
ilsong963 <ilsong963@naver.com>
2025-04-24 17:37:58 +0900

    feat: 지도 및 마커 추가

--- lib/pages/welcome_page/widget/story_marker_map.dart
+++ lib/pages/welcome_page/widget/story_marker_map.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:region_based_chat/models/marker.dart'; // 모델 클래스와 이름 충돌 방지
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class StoryMarkerMap extends ConsumerStatefulWidget {
   const StoryMarkerMap({super.key});
@@ -9,63 +14,113 @@ class StoryMarkerMap extends ConsumerStatefulWidget {
   StoryMarkerMapState createState() => StoryMarkerMapState();
 }
 
-class StoryMarkerMapState extends ConsumerState<StoryMarkerMap> {
-  NaverMapController? _mapController;
+class StoryMarkerMapState extends ConsumerState<StoryMarkerMap> with WidgetsBindingObserver {
+  NaverMapController? mapController;
+  final Set<NMarker> currentMarkers0 = {}; // 현재 지도에 표시된 마커 세트
+  List<Marker> previousMarkers = []; // 이전 조회된 마커 리스트
+  Timer? pollingTimer; // 주기적인 폴링을 위한 타이머
 
-  // Set<NMarker> _buildMarkers(List<Location>? locations) {
-  //   if (locations == null || locations.isEmpty) {
-  //     return <NMarker>{};
-  //   }
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  bool isMapReady = false;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this); // 위젯 바인딩 옵저버 등록
+    // 5초마다 새로운 마커를 확인하는 폴링 시작
+    startPollingTimer();
+  }
 
-  //   return locations.map((location) {
-  //     final marker = NMarker(
-  //       id: 'marker_${removeTag(location.title)}',
-  //       position: convertVWorldToNaverLatLng(location.mapx, location.mapy),
-  //       caption: NOverlayCaption(text: removeTag(location.title)),
-  //     );
+  void startPollingTimer() {
+    pollingTimer ??= Timer.periodic(const Duration(seconds: 5), (timer) {
+      fetchAndUpdateMarkers();
+    });
+  }
 
-  //     _mapController!.addOverlay(marker);
+  void stopPollingTimer() {
+    pollingTimer?.cancel();
+    pollingTimer = null;
+  }
 
-  //     if (location.link.startsWith("https")) {
-  //       final onMarkerInfoWindow = NInfoWindow.onMarker(id: marker.info.id, text: "링크");
-  //       marker.openInfoWindow(onMarkerInfoWindow);
-  //       marker.setOnTapListener((NMarker tappedMarker) {
-  //         Navigator.push(context, MaterialPageRoute(builder: (_) => DetailPage(url: location.link)));
-  //       });
-  //     }
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    log(state.toString());
+    if (state == AppLifecycleState.resumed) {
+      // 화면에 다시 돌아왔을 때 타이머 재개
+      startPollingTimer();
+      // 화면이 다시 활성화되었으므로 데이터를 즉시 갱신할 수도 있습니다.
+      fetchAndUpdateMarkers();
+    } else {
+      // 화면을 보고있는 상태가 아닐때
+      stopPollingTimer();
+    }
+  }
 
-  //     return marker;
-  //   }).toSet();
-  // }
+  Future<List<Marker>> fetchMarkers() async {
+    final snapshot = await firestore.collection('markers').get();
+    return snapshot.docs.map((doc) => Marker.fromFirestore(doc)).toList();
+  }
 
-  // void _updateMarkers(List<Location>? locations) {
-  //   if (_mapController == null) return;
+  NMarker convertToNMarker(Marker marker) {
+    return NMarker(
+      id: marker.id,
+      position: NLatLng(marker.latitude, marker.longitude),
+    );
+  }
 
-  //   // 기존 마커 제거
-  //   _mapController!.clearOverlays();
+  void updateMapMarkers(List<Marker> newMarkers) {
+    if (mapController == null || !isMapReady) return;
 
-  //   // 새로운 마커 추가
-  //   _buildMarkers(locations);
-  // }
+    setState(() {
+      // 기존 마커 제거
+      for (var marker in currentMarkers0) {
+        mapController!.deleteOverlay(marker.info);
+      }
+      currentMarkers0.clear();
+
+      // 새로운 마커 추가
+      for (final marker in newMarkers) {
+        final nMarker = convertToNMarker(marker);
+        mapController!.addOverlay(nMarker);
+        currentMarkers0.add(nMarker);
+      }
+      previousMarkers = newMarkers; // 현재 마커 리스트를 이전 리스트로 업데이트
+    });
+  }
+
+  Future<void> fetchAndUpdateMarkers() async {
+    final currentMarkers = await fetchMarkers();
+
+    // 이전 마커 리스트와 비교하여 새로운 마커가 있는지 확인
+    final newMarkers = currentMarkers.where((marker) => !previousMarkers.any((prevMarker) => prevMarker.id == marker.id)).toList();
+
+    if (newMarkers.isNotEmpty || currentMarkers.length != previousMarkers.length) {
+      // 새로운 마커가 있거나 마커 개수가 변경되었으면 지도 업데이트
+      updateMapMarkers(currentMarkers);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // WidgetsBinding.instance.addPostFrameCallback((_) {
-    //   _updateMarkers(locations);
-    // });
-
     return Stack(
       children: [
         NaverMap(
-          options: NaverMapViewOptions(initialCameraPosition: NCameraPosition(target: NLatLng(37.5, 127.0), zoom: 12)),
           onMapReady: (controller) {
             setState(() {
-              _mapController = controller;
-              // _updateMarkers(locations);
+              mapController = controller;
+              isMapReady = true;
+              // 맵이 준비되면 초기 데이터 로딩
+              fetchAndUpdateMarkers();
             });
           },
+          options: const NaverMapViewOptions(
+            initialCameraPosition: NCameraPosition(
+              target: NLatLng(37.5, 127),
+              zoom: 12,
+            ),
+          ),
         ),
-        Positioned(right: 10, bottom: MediaQuery.of(context).size.height / 2, child: NaverMapZoomControlWidget(mapController: _mapController)),
+        Positioned(right: 10, bottom: MediaQuery.of(context).size.height / 2, child: NaverMapZoomControlWidget(mapController: mapController)),
       ],
     );
   }
