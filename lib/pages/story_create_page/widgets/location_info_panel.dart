d75b11df04d593a67c2e0be12dc4e39a3fb6a657
hippieperm <leess0133@gmail.com>
2025-04-24 21:24:09 +0900

    feat: 위치 정보 패널 위젯 추가 및 선택한 위치 표시 기능 구현

---
+++ lib/pages/story_create_page/widgets/location_info_panel.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
+
+class LocationInfoPanel extends StatelessWidget {
+  final NLatLng? selectedLocation;
+  final VoidCallback onEditPressed;
+
+  const LocationInfoPanel({
+    Key? key,
+    required this.selectedLocation,
+    required this.onEditPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onEditPressed,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 16, top: 8),
+        decoration: BoxDecoration(
+          color: selectedLocation != null
+              ? Colors.blue.withOpacity(0.08)
+              : Colors.grey.withOpacity(0.05),
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+              color: selectedLocation != null
+                  ? Colors.blue.withOpacity(0.2)
+                  : Colors.grey.withOpacity(0.2)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          child: Row(
+            children: [
+              Icon(Icons.location_on,
+                  color:
+                      selectedLocation != null ? Colors.blue : Colors.grey[400],
+                  size: 20),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  selectedLocation != null
+                      ? '선택한 위치: ${selectedLocation!.latitude.toStringAsFixed(5)}, ${selectedLocation!.longitude.toStringAsFixed(5)}'
+                      : '위치를 선택해주세요',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: selectedLocation != null
+                        ? Colors.black87
+                        : Colors.grey[600],
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.edit,
+                color:
+                    selectedLocation != null ? Colors.blue : Colors.grey[400],
+                size: 16,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
