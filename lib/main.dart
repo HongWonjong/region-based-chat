6cb67a898cb7696a9695c383d273ee349aaf3b26
hippieperm <leess0133@gmail.com>
2025-04-24 10:59:14 +0900

    fix: 스토리 생성 페이지의 경로를 수정하여 올바른 위치로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:region_based_chat/pages/story_create_page.dart';
 import 'firebase_options.dart';
+import 'package:region_based_chat/pages/story_create_page/story_create_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
