7de50d624d954232d3812cb54a0f1f77b42c165f
ghghtls <ghghtls2@gmail.com>
2025-04-24 14:31:26 +0900

    feat:구글 로그인 ui구현

---
+++ lib/pages/auth/auth_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final authProvider = StateNotifierProvider<AuthNotifier, User?>((ref) {
+  return AuthNotifier();
+});
+
+class AuthNotifier extends StateNotifier<User?> {
+  AuthNotifier() : super(FirebaseAuth.instance.currentUser);
+
+  Future<void> signInWithGoogle() async {
+    try {
+      final googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return;
+
+      final googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      state = FirebaseAuth.instance.currentUser;
+    } catch (e) {
+      print("로그인 실패: \$e");
+    }
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+    state = null;
+  }
+}
