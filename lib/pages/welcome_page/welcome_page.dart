23b15e28fb0c57d22472d5864a5047fb9d8847b7
hippieperm <leess0133@gmail.com>
2025-04-28 17:06:26 +0900

    feat: 환영 페이지 UI 개선 - 앱바 스타일 업데이트 및 새로고침 버튼 추가

--- lib/pages/welcome_page/welcome_page.dart
+++ lib/pages/welcome_page/welcome_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:region_based_chat/pages/welcome_page/widget/story_bottom_sheet.dart';
 import 'package:region_based_chat/pages/welcome_page/widget/story_marker_map.dart';
 import '../auth/custom_drawer.dart';
+import '../../style/style.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -21,12 +22,41 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Image.asset(
-            'assets/logo_soeasy.png',
-            height: 110,
+          elevation: 4,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  AppBarStyles.appBarGradientStart,
+                  AppBarStyles.appBarGradientEnd,
+                ],
+              ),
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo_soeasy.png',
+                height: 110,
+                // color: Colors.white,
+              ),
+              const SizedBox(width: 8),
+            ],
           ),
           centerTitle: true,
-          backgroundColor: Colors.white,
+          iconTheme: const IconThemeData(color: Colors.white),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {
+                // 지도 새로고침 기능
+              },
+              tooltip: '지도 새로고침',
+            ),
+          ],
         ),
         drawer: const CustomDrawer(), // 로그인 드로어
         body: Stack(
