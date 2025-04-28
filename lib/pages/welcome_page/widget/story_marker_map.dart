9b22ec62c3364d5ff4b06cccd5a0d9d91f3ddf3c
hippieperm <leess0133@gmail.com>
2025-04-29 06:59:51 +0900

    feat: 네이버 맵 줌 컨트롤 위젯 추가 - 확대 및 축소 버튼 구현

--- lib/pages/welcome_page/widget/story_marker_map.dart
+++ lib/pages/welcome_page/widget/story_marker_map.dart
@@ -10,6 +10,100 @@ import '../../../providers/marker_provider.dart';
 import '../util/polling_timer.dart';
 import 'focus_detector.dart';
 
+// 네이버 맵 줌 컨트롤 위젯 구현
+class NaverMapZoomControlWidget extends StatelessWidget {
+  final NaverMapController? mapController;
+
+  const NaverMapZoomControlWidget({super.key, required this.mapController});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // 확대 버튼
+        Container(
+          decoration: BoxDecoration(
+            color: isDark ? Colors.grey[800] : Colors.white,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(8),
+              topRight: Radius.circular(8),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: IconButton(
+            icon: Icon(
+              Icons.add,
+              color: isDark ? Colors.amber : Colors.deepPurple,
+            ),
+            onPressed: () {
+              if (mapController != null) {
+                mapController!.updateCamera(
+                  NCameraUpdate.zoomIn(),
+                );
+              }
+            },
+            padding: const EdgeInsets.all(12),
+            constraints: const BoxConstraints(),
+            iconSize: 24,
+          ),
+        ),
+
+        // 구분선
+        Container(
+          height: 1,
+          color: isDark
+              ? Colors.grey[700]!.withOpacity(0.3)
+              : Colors.grey.withOpacity(0.3),
+          width: 48,
+        ),
+
+        // 축소 버튼
+        Container(
+          decoration: BoxDecoration(
+            color: isDark ? Colors.grey[800] : Colors.white,
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(8),
+              bottomRight: Radius.circular(8),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: IconButton(
+            icon: Icon(
+              Icons.remove,
+              color: isDark ? Colors.amber : Colors.deepPurple,
+            ),
+            onPressed: () {
+              if (mapController != null) {
+                mapController!.updateCamera(
+                  NCameraUpdate.zoomOut(),
+                );
+              }
+            },
+            padding: const EdgeInsets.all(12),
+            constraints: const BoxConstraints(),
+            iconSize: 24,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class StoryMarkerMap extends ConsumerStatefulWidget {
   final DraggableScrollableController draggableController;
 
