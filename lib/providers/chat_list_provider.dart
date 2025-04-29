dc3c38b7f973d0aa04875122d78063bb80ceb372
wonjonghong <wonhong1996@naver.com>
2025-04-29 15:06:02 +0900

    fix: chat_list_page 관련 구조 리팩토링 관련 파일 이름 변경으로 인한 오류 수정

--- lib/providers/chat_list_provider.dart
+++ lib/providers/chat_list_provider.dart
@@ -1,9 +1,8 @@
 // Provider 설정
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../models/chat_item.dart';
+import '../models/chat_item_model.dart';
 import '../pages/chat_list_page/chat_list_view_model.dart';
 
-final chatListProvider = StateNotifierProvider<ChatListViewModel, List<ChatItem>>(
+final chatListProvider = StateNotifierProvider<ChatListViewModel, List<ChatItemModel>>(
       (ref) => ChatListViewModel(),
 );
